4f6e42b528ab6ff74078387eba4e3c432fd55dad
Frédéric DUPERIER <fduperier@gmail.com>
2017-06-05 11:57:06 +0200

    Création du composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PostFormComponent } from './posts/post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/posts/post-form/post-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../shared/post.model';
+
+@Component({
+  selector: 'app-post-form',
+  templateUrl: './post-form.component.html',
+  styleUrls: ['./post-form.component.css']
+})
+export class PostFormComponent implements OnInit {
+  categories = [ 'Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5' ];
+
+  model = new Post(1,
+    'This is my post title',
+    'Lorem ipsum dolor sit amet...',
+    this.categories[0],
+    'My tag');
+
+  submitted = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() { this.submitted = true; }
+
+  // Only for testing
+  get diagnostic() { return JSON.stringify(this.model); }
+
+}
